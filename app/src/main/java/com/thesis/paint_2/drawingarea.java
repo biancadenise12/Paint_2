55129840a51d6903ad25e95fcd81cfd9cfb75bef
Bianca Denise del Puerto <biancadenise.delpuerto@bicol-u.edu.ph>
2015-12-19 23:05:19 +0800

    Initial commit

---
+++ app/src/main/java/com/thesis/paint_2/drawingarea.java
@@ -0,0 +1,15 @@
+package com.thesis.paint_2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by BiancaDenise on 12/19/2015.
+ */
+public class drawingarea extends View {
+
+    public drawingarea(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
